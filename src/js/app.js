bb2ade8b7447c20a82e7d5cb25c2a14651d8f767
Barkov Dima <di_1@inbox.ru>
2018-03-12 17:14:58 +0200

    full.2 hm1

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,6 @@ const slider = require('./common/test.js');
 const jquery = require('../libs/jquery/dist/jquery.min.js');
 const $ = require('jquery');
 const ddsfn = "dsfsdf";
-alert(ddsfn);
+
 
 const sdjk =346;
-alert(sdjk);
-alert(sdjk);
-alert(sdjk);
